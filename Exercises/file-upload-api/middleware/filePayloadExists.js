3462f2313b316327e12a58979a5f58a4a8d42c25
Madawa De Silva <88103017+TerribleCodes@users.noreply.github.com>
2023-01-09 10:08:08 +0530

    Day 49

--- Exercises/file-upload-api/middleware/filePayloadExists.js
+++
@@ -1,6 +0,0 @@
-const filePayloadExists = (req, res, next) => {
-    if(!req.files) return res.status(400).json({status: "error", message: "missing files"});
-    next();
-}
-
-module.exports = filePayloadExists;
