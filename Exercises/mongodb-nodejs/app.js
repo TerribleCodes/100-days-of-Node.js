373696f5e12d09897a4146602e9f5239b974f607
TerribleCodes <madawadesilva9998@gmail.com>
2023-02-24 20:45:27 +0530

    MongoDB and NodeJS Recap

--- Exercises/mongodb-nodejs/app.js
+++ Exercises/mongodb-nodejs/app.js
@@ -11,31 +11,31 @@ const collection = "books";
 const libraryCollection = client.db(dbname).collection(collection);
 
 const connection = async () => {
-    try{
-        await client.connect();
-        console.log(`Successfully connected to the database...`);
-    }catch(err){
-        console.error(`Error occured: ${err}`)
-    }
-}
+  try {
+    await client.connect();
+    console.log(`Successfully connected to the database...`);
+  } catch (err) {
+    console.error(`Error occured: ${err}`);
+  }
+};
 
-const bookdata = [{
-    name: "Charlie and the Chocolate Factory",
-    author: "Rold Dahl", 
-    last_updated: new Date()
-}]
+const bookdata = {
+  name: "Charlie and the Chocolate Factory",
+  author: "Rold Dahl",
+  last_updated: new Date(),
+};
 
 const main = async () => {
-    try{
-        await connection();
-        let result = await libraryCollection.insertOne(bookdata);
-        // to use the insertMany, send the required documents as an array of BSON documents
-        console.log(`Inserted to ${result.insertedId}`)
-    }catch(err){
-        console.log(`Error occured : ${err}`);
-    }finally{
-        await client.close();
-    }
-}
+  try {
+    await connection();
+    let result = await libraryCollection.insertOne(bookdata);
+    // to use the insertMany, send the required documents as an array of BSON documents
+    console.log(`Inserted to ${result.insertedId}`);
+  } catch (err) {
+    console.log(`Error occured : ${err}`);
+  } finally {
+    await client.close();
+  }
+};
 
 main();
